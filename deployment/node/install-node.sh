944e1eb9b5d57141a3df4397711134a7463192fd
Kavis1 <kavisplayer@gmail.com>
2025-10-07 18:42:25 +0200

    Add ARM64/ARM architecture support for Go installation

--- deployment/node/install-node.sh
+++ deployment/node/install-node.sh
@@ -3,9 +3,9 @@
 set -e
 
 # VERSION INFO - Updated automatically
-SCRIPT_VERSION="352c919"
-SCRIPT_DATE="2025-10-07 19:35 UTC"
-LAST_CHANGE="CRITICAL FIX: Binary path corrected"
+SCRIPT_VERSION="arm64-fix"
+SCRIPT_DATE="2025-10-07 19:40 UTC"
+LAST_CHANGE="Add ARM64 architecture support"
 
 # Цвета
 RED='\033[0;31m'
